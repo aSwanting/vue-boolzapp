0ec023b95505098b497de465b8cd66956faec0d2
aSwanting <gab.damico@gmail.com>
2023-10-26 19:45:29 +0200

    add: send message, random response of 6 preset

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ createApp({
         return {
             contacts,
             currentContact: 0,
+            newMessage: "",
         }
     },
 
@@ -15,7 +16,45 @@ createApp({
 
         selectContact(index) {
             this.currentContact = index
-        }
+        },
+
+        sendMessage() {
+
+            // If valid, push to array
+            if (this.newMessage) {
+
+                this.contacts[this.currentContact].messages.push({
+                    date: "...",
+                    message: this.newMessage,
+                    status: 'sent'
+                })
+
+                this.newMessage = ""
+            }
+        },
+
+        receiveMessage() {
+
+            const returnMessage = [
+                "Ok!",
+                "Sounds good!",
+                "Not so sure..",
+                "You tell me!",
+                "Haha!",
+                "I'm hungry"
+            ]
+
+            const n = Math.floor(Math.random() * returnMessage.length)
+
+            setTimeout(() => {
+                this.contacts[this.currentContact].messages.push({
+                    date: "...",
+                    message: returnMessage[n],
+                    status: 'received'
+                })
+            }, 1000)
+
+        },
 
     },
 
