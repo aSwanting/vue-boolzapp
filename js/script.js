4145f0d66f165b03ad07f0e1f79b659e8a589e20
aSwanting <gab.damico@gmail.com>
2023-10-26 16:06:01 +0200

    add: basic app layout, hardstop gradient bg

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+
+    // Vue Data
+    data() {
+        return {
+
+        }
+    },
+
+    // Vue Methods
+    methods: {
+
+    },
+
+    // Vue Lifecycle
+    mounted() {
+        console.log("It's Alive!")
+    }
+}).mount("#app")
