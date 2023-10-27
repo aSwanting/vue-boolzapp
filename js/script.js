35a27e02ab42041969f46b808822dd52a5d0fff4
aSwanting <gab.damico@gmail.com>
2023-10-27 10:47:03 +0200

    ref: cleanup, unsused functions removed, comments

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ createApp({
         },
 
         sendMessage() {
-
             // If valid, push to array
             if (this.newMessage) {
 
@@ -48,15 +47,11 @@ createApp({
                 this.messageSent = true
                 this.newMessage = ""
             }
-
         },
 
         receiveMessage() {
-
             if (this.messageSent) {
-
                 const responseIndex = Math.floor(Math.random() * this.randomResponses.length)
-
                 setTimeout(() => {
                     this.contacts[this.currentContact].messages.push({
                         date: (new Date().toLocaleString()),
@@ -64,85 +59,50 @@ createApp({
                         status: 'received'
                     })
                 }, 1000)
-
                 this.messageSent = false
-
             }
-
-
         },
 
         convertDate(date) {
-
             const splitDate = date.split("/")
             const convertedDate = `${splitDate[1]}/${splitDate[0]}/${splitDate[2]}`
             return convertedDate
-
-        },
-
-        getCurrentTime() {
-
-            const currentTime = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-            return currentTime
         },
 
         getTimeFromArray(index) {
-
             const convertedDate = this.convertDate(this.contacts[this.currentContact].messages[index].date)
             const time = new Date(convertedDate).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
             return time
-
         },
 
         getReceivedMessages(index) {
-
             const messages = this.contacts[index].messages
             let receivedMessages = []
-
             messages.forEach(message => {
                 if (message.status === "received") {
                     receivedMessages.push(message)
                 }
             })
-
             return receivedMessages
-
         },
 
         printLastMessage(index) {
-
             const receivedMessages = this.getReceivedMessages(index)
             const lastMessage = receivedMessages[receivedMessages.length - 1].message
             return lastMessage
-
         },
 
         printLastMessageTime(index) {
-
             const receivedMessages = this.getReceivedMessages(index)
             const convertedDate = this.convertDate(receivedMessages[receivedMessages.length - 1].date)
             const time = new Date(convertedDate).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
             return time
-
         },
-
-        // contactSearch() {
-        //     this.contacts.forEach(contact => {
-        //         if (!(contact.name.toLowerCase().includes(this.contactSearched))) {
-        //             contact.visible = false
-        //         } else {
-        //             contact.visible = true
-        //         }
-        //     })
-        // },
-
     },
 
     // Vue Lifecycle
     mounted() {
-
         console.log("It's Alive!")
-
     }
 
 }).mount("#app")
